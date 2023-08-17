8c6a616a312bd299a22aa3b0c573730c4deef97b
Lucía Vásquez <bvlucia34@gmail.com>
2023-08-16 22:05:18 -0600

    first commit

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+    import PokemonOptions from '@/components/PokemonOptions.vue'; 
+    import PokemonPicture from '@/components/PokemonPicture.vue';
+
+    export default {
+        components: { PokemonOptions, PokemonPicture }
+    }
+</script>
+
+<template>
+    <h1>¿Quién es este pokémon?</h1>
+    <PokemonPicture :pokemonId="35" :showPokemon="false"/>
+    <PokemonOptions />
+</template>
+
+<style>
+
+</style>
